9f0e3279242f5499b5dc0c836358a009d28cb139
mark stahl <markstahl0219@gmail.com>
2020-05-17 22:32:56 -0400

    MVP

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,32 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import {render, waitFor, fireEvent} from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const {getByText} = render(<CheckoutForm /> );
+    expect(getByText("Checkout Form")).toBeTruthy();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+    const fillform = {
+        firstName: "Mark",
+        lastName: "Stahl",
+        address: "600 Broad St",
+        city: "Riverton",
+        state: "NJ",
+        zip:"08077"
+    }
+    const {getByText, getByTestId} = render( <CheckoutForm /> );
+    await waitFor(() => {
+        fireEvent.input(getByText(/First Name/i), `${fillform.firstName}`);
+        fireEvent.input(getByText(/Last Name/i), `${fillform.lastName}`);
+        fireEvent.input(getByText(/Address/i), `${fillform.address}`);
+        fireEvent.input(getByText(/City/i), `${fillform.city}`);
+        fireEvent.input(getByText(/State/i), `${fillform.state}`);
+        fireEvent.input(getByText(/Zip/i), `${fillform.zip}`);
+        fireEvent.click(getByTestId("checkout"));
+    });
+    expect(getByTestId("successMessage")).toBeTruthy();
+});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,31 @@
-// write your custom hook here to control your checkout form
+import { useLocalStorage } from "./useLocalStorage";
+import React, { useState } from "react";
+
+export const useForm = (initialState, key) => {
+
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [values, setValues] = useLocalStorage(
+    initialState,
+    key
+  );
+
+  const handleChanges = e => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    });
+  };
+
+
+  const clearForm = () => {
+    setValues(initialState);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+    clearForm();
+   };
+
+  return [values, handleChanges, clearForm, handleSubmit];
+};
