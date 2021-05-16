9e2c7893b9fcd39749e5eb542c4a1c1a110a63a1
Mark Stahl <markstahl0219@gmail.com>
2021-05-16 11:52:33 -0400

    mvp and tests passing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,34 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
-// Write up the two tests here and make sure they are testing what the title shows
+test("form header renders", () => {
+  render(<CheckoutForm />);
+  const header = screen.getByText(/checkout form/i);
+  expect(header).toBeInTheDocument();
+});
 
-test("form header renders", () => {});
+test("form shows success message on submit with form details", () => {
+  render(<CheckoutForm />);
 
-test("form shows success message on submit with form details", () => {});
+  const firstName = "Mark";
+  const lastName = "Stahl";
+
+  const firstNameField = screen.getByLabelText(/first name/i);
+  const lastNameField = screen.getByLabelText(/last name/i);
+  const checkoutButton = screen.getByText("Checkout");
+
+  expect(firstNameField).toBeInTheDocument();
+  expect(lastNameField).toBeInTheDocument();
+  expect(checkoutButton).toBeInTheDocument();
+
+  userEvent.type(firstNameField, firstName);
+  userEvent.type(lastNameField, lastName);
+  userEvent.click(checkoutButton);
+
+  const checkoutMessage = screen.getByText(/you have ordered/i);
+  const checkoutName = screen.getByText(/mark stahl/i);
+  expect(checkoutName).toBeInTheDocument();
+  expect(checkoutMessage).toBeInTheDocument();
+});
